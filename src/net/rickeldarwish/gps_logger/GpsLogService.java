b668a09150f97e3b47d9b08f24e4ce5d0d5307fb
Rick El-Darwish <rick@stnoble.com>
2013-10-15 22:55:24 +0200

    Working version with service

---
+++ src/net/rickeldarwish/gps_logger/GpsLogService.java
@@ -0,0 +1,108 @@
+package net.rickeldarwish.gps_logger;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
+
+import android.app.Service;
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.IBinder;
+
+public class GpsLogService extends Service {
+	
+	BufferedWriter log;
+	Handler logHandler;
+	boolean handlerEnabled = false;
+	LocationManager locationMgr;
+	LocationListener locationLsnr;
+	
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		
+		locationMgr = (LocationManager)getSystemService(android.content.Context.LOCATION_SERVICE);
+		locationLsnr = new LocationListener() {
+
+			public void onStatusChanged(String p1, int p2, Bundle p3)
+			{
+				// TODO: Implement this method
+			}
+
+			public void onProviderEnabled(String p1)
+			{
+				// TODO: Implement this method
+			}
+
+			public void onProviderDisabled(String p1)
+			{
+				// TODO: Implement this method
+			}
+			
+			public void onLocationChanged(Location l) {
+				try
+				{
+					logLocation(l);
+				}
+				catch (IOException e)
+				{}
+			}
+			
+		};
+		
+		Date date = new Date();
+		File dir = Environment.getExternalStorageDirectory();
+		File log_file = new File(dir, date.getTime() + ".txt");
+		try {
+			log = new BufferedWriter(new FileWriter(log_file));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}//openFileOutput(date.toString() + ".txt", android.content.Context.MODE_WORLD_WRITEABLE);
+		Criteria locCriteria = new Criteria();
+		locationMgr.requestLocationUpdates(15, 0, locCriteria, locationLsnr, null);
+		
+		return Service.START_NOT_STICKY;
+	}
+	
+	@Override
+	public void onDestroy() {
+		try {
+			log.flush();
+			log.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		locationMgr.removeUpdates(locationLsnr);
+	}
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	private void logLocation(Location l) throws java.io.IOException {
+		//get location
+		Date date = new Date();
+		double latitude = l.getLatitude();
+		double longitude = l.getLongitude();
+		float bearing = l.getBearing();
+		float speed = l.getSpeed();
+		String line = "{'time': '" + date.getTime() +
+		"','lat': " + latitude + ", 'long': " + longitude +
+		", 'brng': " + bearing + ", 'speed': " + speed + "}";
+		//save location to file
+		log.write(line);
+		log.flush();
+	}
+
+}
--- src/net/rickeldarwish/gps_logger/MainActivity.java
+++ src/net/rickeldarwish/gps_logger/MainActivity.java
@@ -1,17 +1,51 @@
 package net.rickeldarwish.gps_logger;
 
 import android.app.*;
+import android.content.Context;
+import android.content.Intent;
 import android.os.*;
 import android.view.*;
+import android.view.View.*;
 import android.widget.*;
 
 public class MainActivity extends Activity
 {
+	
+	Intent serviceIntent;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
 	{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		
+		ToggleButton toggleGPS = (ToggleButton)findViewById(R.id.toggleGPS);
+		toggleGPS.setOnClickListener(new OnClickListener() {
+			public void onClick(View p1) {
+				try {
+					if (((ToggleButton)p1).isChecked())
+						startGPS();
+					else
+						stopGPS();
+				}
+				catch (java.io.IOException e) {}
+			}
+		});
     }
+	
+	private void startGPS() throws java.io.IOException {
+		TextView txt = (TextView)findViewById(R.id.flavorText);
+		txt.setText("Logging... Press the button to stop.");
+		Context context = getBaseContext();
+		serviceIntent = new Intent(context, GpsLogService.class);
+		context.startService(serviceIntent);
+	}
+	
+	private void stopGPS() throws java.io.IOException{
+		TextView txt = (TextView)findViewById(R.id.flavorText);
+		txt.setText("Welcome to the GPS Logger. Press the button to start logging.");
+		Context context = getBaseContext();
+		context.stopService(serviceIntent);
+	}
 }
